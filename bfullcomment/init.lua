f982d980e7da5df1a294f986bf2973362d2c3fab
Maduki-tech <maduki.business@gmail.com>
2021-09-14 16:45:03 +0200

    all comments for js

--- bfullcomment/init.lua
+++ bfullcomment/init.lua
@@ -1,5 +1,46 @@
 local api = vim.api
 
+local function createLineComment(langu, line)
+
+	if langu == "javascript" or langu == "typescript" or langu == "typescriptreact" or langu == "javascriptreact" then
+		local commentStart = "/*"
+		local commentLength = 76
+		local spacerComment = "*"
+		local commentEnd = "*/"
+		local center = ""
+
+		-- Handle Comment length with Word
+		line = " " .. line .. " "
+		local cLength = string.len(line)
+		-- if string is 2 4 6 8
+		if (cLength % 2 == 0) then
+			cLength = string.len(line)
+		end
+
+
+		local spaceLength = ((commentLength - cLength) / 2)
+
+		for _ = 1, spaceLength, 1 do
+			center = center .. spacerComment
+		end
+
+		local buildString = commentStart .. center .. line .. center .. commentEnd
+
+		api.nvim_put({buildString}, 'l', false, false)
+
+
+	end
+end
+
+
+local function simpleComment(langu, line)
+	if langu == "javascript" or langu == "typescript" or langu == "typescriptreact" or langu == "javascriptreact" then
+		local comment = "// "
+		local simple = comment .. line
+		api.nvim_put({simple}, 'l', false, false)
+	end
+end
+
 local function createComment(langu, line)
 	if langu == "javascript" or langu == "typescript" or langu == "typescriptreact" or langu == "javascriptreact" then
 		local commentStart = "/*"
@@ -9,7 +50,6 @@ local function createComment(langu, line)
 		local spacer = ""
 		local placerholder = " "
 		local center = ""
-
 
 		-- GET THE HALF LENGTH WITH COMMENT
 		local cLength = string.len(line)
@@ -22,22 +62,17 @@ local function createComment(langu, line)
 
 		local spaceLength = ((commentLength - cLength) / 2)
 
-		for i = 1, commentLength, 1 do
+		for _ = 1, commentLength, 1 do
 			spacer = spacer .. spacerComment
 		end
 
-		for i = 1, spaceLength, 1 do
+		for _ = 1, spaceLength, 1 do
 			center = center .. placerholder
 		end
-
-
 
 		local commandLineStart = commentStart .. spacer
 		local buildString = spacerComment .. center .. line .. center .. spacerComment
 		local commandLineEnd = spacer .. commentEnd
-
-
-
 
 		api.nvim_put({commandLineEnd}, 'l', false, false)
 		api.nvim_put({buildString}, 'l', false, false)
@@ -53,10 +88,30 @@ local function getText()
 
 	api.nvim_del_current_line()
 
+	return filetype, currentline
+end
+
+local function HeaderComment()
+	local filetype, currentline = getText()
+
 	createComment(filetype, currentline)
 end
 
 
+
+local function LineSeperator()
+	local filetype, currentline = getText()
+	createLineComment(filetype, currentline)
+end
+
+local function simpleOne()
+	local filetype, currentline = getText()
+	simpleComment(filetype, currentline)
+end
+
+-- ALL RETURN FUNTIONS TO CALL IN VIM
 return{
-	getText = getText
+	HeaderComment = HeaderComment,
+	LineSeperator = LineSeperator,
+	simpleOne = simpleOne
 }
